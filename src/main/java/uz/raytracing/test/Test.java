fb02e9551cbc163066e19143759b99b9eb7f47f9
Lazizkhan1 <lazizkhanhadjaye@gmail.com>
2023-09-04 06:39:35 +0500

    All stuff made here and implemented Application.java :)

--- src/main/java/uz/raytracing/test/Test.java
+++ src/main/java/uz/raytracing/test/Test.java
@@ -1,60 +1,27 @@
 package uz.raytracing.test;
 
-import uz.raytracing.util.Viewport;
+import uz.raytracing.components.Frame;
+import uz.raytracing.components.Panel;
+import uz.raytracing.components.SplitPane;
+import uz.raytracing.components.Viewport;
 
-import javax.swing.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.io.*;
-import java.util.Properties;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+
 
 public class Test {
     public static void main(String[] args) {
-        Viewport viewport = new Viewport();
-        JPanel controlPanel = new JPanel();
         JButton button = new JButton("Render");
         JLabel label = new JLabel("Framerate");
-        JFrame frame = new JFrame("Test");
-        File fileProperty = getPropertyFile();
+        Panel controlPanel = new Panel(label, button);
+        Viewport viewport = new Viewport();
+        SplitPane splitPane = new SplitPane(viewport, controlPanel);
+        Frame frame = new Frame("Test");
 
-
-
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
-        frame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                try {
-                    File propertyFile = getPropertyFile();
-                    FileOutputStream os = new FileOutputStream("Laziz");
-                    Properties properties = new Properties();
-                    properties.put("FrameWidth", frame.getWidth() + "");
-                    properties.put("FrameHeight", frame.getHeight() + "");
-                    properties.put("ScreenLocationX", frame.getLocationOnScreen().x + "");
-                    properties.put("ScreenLocationY", frame.getLocationOnScreen().y + "");
-                    os.close();
-                    properties.store(os, "Main screen configuration");
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                } finally {
-                  super.windowClosing(e);
-                }
-            }
-        });
-
-        controlPanel.setLayout(new BoxLayout(controlPanel, BoxLayout.Y_AXIS));
-        controlPanel.add(label);
-        controlPanel.add(button);
-
-        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, viewport, controlPanel);
-
-        frame.add(splitPane);
-
+        frame.add(splitPane, BorderLayout.CENTER);
         frame.setVisible(true);
 
     }
 
-    public static File getPropertyFile() {
-        return new File("./src/main/java/uz/raytracing/config/application.properties");
-    }
 }
